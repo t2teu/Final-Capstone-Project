c17d27d0eb01a3abe433e6d70b22c0c0becad8e6
teteu <msouza11302003@gmail.com>
2023-06-23 15:51:32 -0700

    Update v0.1.5
    Reorganized files and made the first API call to make posts readable

--- scripts/garden.js
+++ scripts/garden.js
@@ -1,34 +1,42 @@
-fetch('https://microbloglite.herokuapp.com/api/posts')
-      .then(response => response.json())
-      .then(data => {
-        const postsContainer = document.getElementById('posts-container');
-        const posts = data.posts;
+// fetch api
+fetch('https://microbloglite.herokuapp.com/api/posts?limit=100000000000&offset=0', {
+    method: 'GET',
+    headers: {
+      'accept': 'application/json',
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InN0cmluZyIsImlhdCI6MTY4NzU1MzY1NiwiZXhwIjoxNjg3NjQwMDU2fQ.v5_q7pJnJg51f4UnZgo_d3EQpPGPvD2M8iDPkZLwDvo'
+    }
+  })
+    .then(response => response.json())
+    .then(data => {
+     const postsContainer = document.getElementById('posts');
+      const posts = data;
 
+      if (posts) {
         posts.forEach(post => {
           const postElement = document.createElement('div');
-          postElement.classList.add('post');
+          postElement.className = 'post';
 
-          const usernameElement = document.createElement('span');
-          usernameElement.classList.add('username');
+          const usernameElement = document.createElement('div');
+          usernameElement.className = 'username';
           usernameElement.textContent = post.username;
 
-          const timestampElement = document.createElement('span');
-          timestampElement.classList.add('timestamp');
-          timestampElement.textContent = new Date(post.createdAt).toLocaleString();
-
-          const likesElement = document.createElement('span');
-          likesElement.classList.add('likes');
-          likesElement.textContent = `Likes: ${post.likes.length}`;
-
-          const textElement = document.createElement('p');
+          const textElement = document.createElement('div');
           textElement.textContent = post.text;
 
+          const likesElement = document.createElement('div');
+          likesElement.className = 'likes';
+          likesElement.textContent = `Likes: ${post.likes.length}`;
+
           postElement.appendChild(usernameElement);
-          postElement.appendChild(timestampElement);
-          postElement.appendChild(likesElement);
           postElement.appendChild(textElement);
+          postElement.appendChild(likesElement);
 
           postsContainer.appendChild(postElement);
         });
-      })
-      .catch(error => console.error(error));
+      } else {
+        console.error('No posts found');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
