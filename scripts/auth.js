a91bf0c0efa9cfebe8cddf869b761ae29a6135eb
teteu <msouza11302003@gmail.com>
2023-06-23 13:47:01 -0700

    Update v0.1.3
    Began login page directed to garden page, as well as supplied token

--- scripts/auth.js
+++ scripts/auth.js
@@ -43,8 +43,7 @@ function login (loginData) {
         .then(response => response.json())
         .then(loginData => {
             window.localStorage.setItem("login-data", JSON.stringify(loginData));
-            window.location.assign("/posts");  // redirect
-
+            window.location.assign("garden.html");  // redirect
             return loginData;
         });
 }
