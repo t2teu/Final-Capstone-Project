af78b912b38976c69e2c9257ee63d32abacc2f6a
Jenae Codes <californiacollege.jennifer@gmail.com>
2023-06-26 13:43:07 -0700

    CSS to Alert Messages

--- scripts/registration.js
+++ scripts/registration.js
@@ -11,12 +11,18 @@ function signUpUser(event) {
 
   // validates the length of the password
   if (password.length < 8) {
-    return alert("Password must be at least 8 characters");
+    return alert("Password must be at least 8 characters", "alert-warning-custom");
   }
 
   // makes sure that passwords match
   if (password !== confirmPassword) {
-    return alert("Passwords do not match");
+    return alert("Passwords do not match", "alert-warning-custom");
+  }
+
+    // this will check if the terms of service checkbox is checked
+  const termsCheck = document.getElementById('agree-term');
+  if (!termsCheck.checked) {
+    return alert("Please agree to the terms of service", "alert-info-custom");
   }
 
   const headers = new Headers(); // new headers
@@ -41,13 +47,13 @@ function signUpUser(event) {
     .then(response => {
       if (response.ok) { // HTTP status code 200-299 for a successful response
         console.log("User registered successfully!");
-        alert("User registration created successfully!");
+        alert("User registration created successfully!", "alert-success-custom");
         window.location.href = '/html/login.html'; // this redirects the user to the login
       } else {
         if (response.status === 400) { // 400 if response is not successful
           return response.json().then(data => {
             if (data.message === "Username already taken") {
-              alert("Username is already taken!");
+              alert("Username is already taken!", "alert-danger-custom");
             } else {
               throw new Error("Registration failed!");
             }
@@ -58,4 +64,12 @@ function signUpUser(event) {
       }
     })
     .catch(error => console.log(error)); // handle errors occurring during the request
+}
+function showAlert(message, alertClass) {
+  const alertContainer = document.createElement('div');
+  alertContainer.className = `alert alert-custom ${alertClass}`;
+  alertContainer.textContent = message;
+
+  const form = document.getElementById('signUpForm');
+  form.insertAdjacentElement('beforebegin', alertContainer);
 }
