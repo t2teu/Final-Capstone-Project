3939478d8a48251c305cda085589ac0883976a1b
Jenae Codes <californiacollege.jennifer@gmail.com>
2023-06-26 11:52:31 -0700

    Added comments to JS, CSS HTML
    Comments and CSS

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,47 +1,50 @@
 "use strict";
 
-function registerUser(event) {
-  event.preventDefault();
+function signUpUser(event) {
+  event.preventDefault(); // prevents the default form behavior on the submission
 
-  const usernameInput = document.getElementById('username').value;
-  const fullnameInput = document.getElementById('fullname').value;
-  const passwordInput = document.getElementById('password').value; 
-  const confirmPasswordInput = document.getElementById('cpassword').value; 
+  // Get user input values from the form
+  const username = document.getElementById('username').value;
+  const fullname = document.getElementById('fullname').value;
+  const password = document.getElementById('password').value; 
+  const confirmPassword = document.getElementById('cpassword').value; 
 
-  // Add validation for password length
-  if (passwordInput.length < 8) {
+  // validates the length of the password
+  if (password.length < 8) {
     return alert("Password must be at least 8 characters");
   }
 
-  // Add validation for password match
-  if (passwordInput !== confirmPasswordInput) {
+  // makes sure that passwords match
+  if (password !== confirmPassword) {
     return alert("Passwords do not match");
   }
 
-  let myHeaders = new Headers();
-  myHeaders.append("Content-Type", "application/json");
+  const headers = new Headers(); // new headers
+  headers.append("Content-Type", "application/json"); // sets the "Content-Type" header
 
-  let raw = JSON.stringify({
-    "username": usernameInput,
-    "fullName": fullnameInput,
-    "password": passwordInput,
+  // changing user info to JSON
+  const reqBody = JSON.stringify({
+    "username": username,
+    "fullName": fullname,
+    "password": password,
   });
 
-  let requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: raw,
-    redirect: 'follow'
+  const reqOpt = {
+    method: 'POST', // makes the HTTP method as POST
+    headers: headers, // headers for the request
+    body: reqBody, // setting request body
+    redirect: 'follow' // redirecting option
   };
 
-  fetch("https://microbloglite.herokuapp.com/api/users", requestOptions)
+  // sending out a POST request to API endpoint
+  fetch("https://microbloglite.herokuapp.com/api/users", reqOpt)
     .then(response => {
-      if (response.ok) {
+      if (response.ok) { // HTTP status code 200-299 for a successful response
         console.log("User registered successfully!");
         alert("User registration created successfully!");
-        window.location.href = '/html/login.html';
+        window.location.href = '/html/login.html'; // this redirects the user to the login
       } else {
-        if (response.status === 400) {
+        if (response.status === 400) { // 400 if response is not successful
           return response.json().then(data => {
             if (data.message === "Username already taken") {
               alert("Username is already taken!");
@@ -54,5 +57,5 @@ function registerUser(event) {
         }
       }
     })
-    .catch(error => console.log(error)); 
+    .catch(error => console.log(error)); // handle errors occurring during the request
 }
