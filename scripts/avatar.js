c8c6fc6297d7f113e49dca3d518d4685e9c2621b
leannabonalba <80809177+leannabonalba@users.noreply.github.com>
2023-06-27 11:08:41 -0700

    Display logged in username in navbar

---
+++ scripts/avatar.js
@@ -0,0 +1,24 @@
+"use strict";
+
+window.onload = function() {
+    displayUserNameAndProfile();
+};
+
+
+
+function displayUserNameAndProfile() {
+    //Get display div
+    let usernameDiv = document.getElementById("userHere");
+    //Get user's username
+    let loginData = getLoginData();
+    let username = loginData.username;
+
+    //Change text content to username
+    usernameDiv.textContent = username;
+
+    //Getting Profile Image from Gravatar
+}
+
+// function createHash(username){
+//     let hash = username.trim().toLowerCase();
+// }
