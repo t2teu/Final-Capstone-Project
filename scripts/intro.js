6e4bcfcf55680487c282236c02b6d2885d78555f
teteu <msouza11302003@gmail.com>
2023-06-27 13:30:09 -0700

    Update v0.2.5
    Added intro.html page and styling

---
+++ scripts/intro.js
@@ -0,0 +1,11 @@
+const gradient = document.querySelector( '.gradient' );
+
+function getPos( ev ){
+  gradient.style.background = 'radial-gradient( circle at ' 
+    + ev.clientX + 'px '
+    + ev.clientY + 'px, '
+    + 'rgba( 0, 80, 120, 0.7) 0, '
+    + 'rgba( 0, 0, 20, 1 ) 80% )';
+}
+addEventListener( 'mousemove', getPos, false );
+
