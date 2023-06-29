bdfd0c6b2a2cc219fb21b2ec35e544efdb26110e
Jenae Codes <californiacollege.jennifer@gmail.com>
2023-06-29 00:59:09 -0700

    NavBar Click Hover

--- scripts/dropup.js
+++ scripts/dropup.js
@@ -3,7 +3,7 @@
 
 function toggleDropUpContent(event) {
     event.preventDefault();
-    var dropUpContent = event.target.nextElementSibling;
+    let dropUpContent = event.target.nextElementSibling;
     dropUpContent.style.display = (dropUpContent.style.display === 'block') ? 'none' : 'block';
   }
   
